1be7ab2d76a48d8e1458f0b2dfa584035fcb8a09
Juho Eerola <juho.eerola@tuni.fi>
2019-11-25 16:35:55 +0200

    Day 1, part 2

--- day_01/solution.py
+++ day_01/solution.py
@@ -1,5 +1,18 @@
+from itertools import cycle
 
 with open('input.txt') as f:
 	frequencies = [int(line) for line in f]
 
 print('Day 1, part 1:', sum(frequencies))
+
+repeated_frequensies = cycle(frequencies)
+
+total = 0
+seen = {total}
+for change in repeated_frequensies:
+	total += change
+	if total in seen:
+		print('Day 1, part 2:', total)
+		break
+	seen.add(total)
+
