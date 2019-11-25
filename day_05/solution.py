f6caf0062518376c822e5219150c164de4b9a38f
Juho Eerola <juho.eerola@tuni.fi>
2019-11-25 20:31:24 +0200

    Day 5, part 1

---
+++ day_05/solution.py
@@ -0,0 +1,30 @@
+
+def polymerise(string):
+	return [ord(c.upper()) * [-1, 1][c.isupper()] for c in string]
+
+
+with open('input.txt') as f:
+	polymer = polymerise(f.read().strip())
+
+# polymer = polymerise('dabAcCaCBAcCcaDA')
+
+
+def react(polymer):
+	reacted = True
+	i = 1
+	while reacted:
+		reacted = False
+		while i < len(polymer):
+			if i > 0 and polymer[i] + polymer[i-1] == 0:
+				reacted = True
+				polymer.pop(i)
+				polymer.pop(i - 1)
+				i = i - 1
+			else:
+				i += 1
+	return polymer
+
+
+reacted = react(polymer)
+print('Day 5, part 1:', len(reacted))
+
