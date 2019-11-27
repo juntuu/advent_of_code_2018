68614439f60b26524c09c7fb4b2bcafab0fa2d1c
Juho Eerola <juho.eerola@tuni.fi>
2019-11-27 12:32:41 +0200

    Day 7, part 2

--- day_07/solution.py
+++ day_07/solution.py
@@ -1,4 +1,7 @@
 
+from collections import defaultdict
+from copy import deepcopy
+
 with open('input.txt') as f:
 	edges = [(field[1], field[7]) for field in map(str.split, f)]
 
@@ -6,6 +9,8 @@ steps = set(sum(edges, tuple()))
 todo = {step: set() for step in steps}
 for a, b in edges:
 	todo[b].add(a)
+
+copy = deepcopy(todo)
 
 done = ''
 while todo:
@@ -16,4 +21,25 @@ while todo:
 	done += step
 
 print('Day 7, part 1:', done)
+
+todo = copy
+workers = 1 + 5
+second = 0
+free = defaultdict(set)
+while todo:
+	ready = free.get(second, set())
+	workers += len(ready)
+	for s0 in ready:
+		for s1 in todo.values():
+			s1.discard(s0)
+	available = sorted(s for s, pre in todo.items() if not pre)
+	for step in available:
+		if workers < 1:
+			break
+		workers -= 1
+		free[second + ord(step) - 4].add(step)
+		todo.pop(step)
+	second += 1
+
+print('Day 7, part 2:', max(free))
 
