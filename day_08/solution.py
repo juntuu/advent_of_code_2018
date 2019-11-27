5f58c1e5d62102f18a9d49fc3991757f75373c84
Juho Eerola <juho.eerola@tuni.fi>
2019-11-27 12:37:26 +0200

    Day 8, part 2

--- day_08/solution.py
+++ day_08/solution.py
@@ -1,5 +1,5 @@
 
-from collections import namedtuple
+from collections import namedtuple, Counter
 
 with open('input.txt') as f:
 	license = list(map(int, f.read().split()))
@@ -26,4 +26,18 @@ tree, rest = parse(license)
 assert rest == []
 
 print('Day 8, part 1:', tree_sum(tree))
+
+
+def tree_value(node):
+	children, meta = node
+	if not children:
+		return sum(meta)
+	counts = Counter(meta)
+	return sum(
+			tree_value(children[i-1]) * factor
+			for i, factor in counts.items()
+			if i-1 in range(len(children)))
+
+
+print('Day 8, part 2:', tree_value(tree))
 
