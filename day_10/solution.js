824fe6a9b4129d2e72f5389257c9458a9aa49344
Juho Eerola <juho.eerola@tuni.fi>
2019-11-27 23:11:39 +0200

    Day 10, part 1
    This time with JavaScript and HTML canvas.

---
+++ day_10/solution.js
@@ -0,0 +1,68 @@
+
+
+const points = []
+
+const advance = k => {
+	let minx = 0
+	let miny = 0
+	second += k
+	points.forEach(p => {
+		p.x += p.dx * k
+		p.y += p.dy * k
+		if (p.x < minx) minx = p.x
+		if (p.y < miny) miny = p.y
+	})
+	return { x: minx, y: miny, }
+}
+
+const width = 1200
+const height = 800
+const ctx = document.getElementById('canvas').getContext('2d')
+const info = document.getElementById('info')
+let k = 0
+let dir = 1
+let pause = true
+
+document.addEventListener('keydown', event => {
+	switch (event.key) {
+		case ' ': pause = !pause; break
+		case '+': if (k < 20) k++; break
+		case '-': if (k > 0) k--; break
+		case '0': k = 0; break
+		case 'ArrowLeft': dir = -1; break
+		case 'ArrowRight': dir = 1; break
+		case 'ArrowUp': if (pause) advance(1); break
+		case 'ArrowDown': if (pause) advance(-1); break
+	}
+}, false);
+
+function draw() {
+	const speed = k * dir
+	info.textContent = `${pause ? '||' : '>'} speed: ${speed} second: ${second}`
+	const min = advance(speed * !pause)
+	ctx.clearRect(0, 0, width, height)
+	ctx.save()
+	ctx.scale(4, 4)
+	points.forEach(({x, y}) => ctx.fillRect(x - min.x, y - min.y, 1, 1))
+	ctx.restore()
+	window.requestAnimationFrame(draw)
+}
+
+const regex = RegExp(/[^<]*<\s*([-\d]+),\s*([-\d]+)>\s*[^<]*<\s*([-\d]+),\s*([-\d]+)>\s*/)
+input.split('\n').forEach(line => {
+	match = regex.exec(line.trim())
+	if (match && match.length != 5) {
+		console.error(match, line)
+	}
+	if (match) {
+		points.push({
+			x: parseInt(match[1]),
+			y: parseInt(match[2]),
+			dx: parseInt(match[3]),
+			dy: parseInt(match[4]),
+		})
+	}
+})
+
+window.requestAnimationFrame(draw)
+
