7f9b14ff6dbcd132d9c1a519f71b7229f2b981f4
Juho Eerola <juho.eerola@tuni.fi>
2019-11-30 23:35:42 +0200

    Day 14, part 2
    A brute force solution, finishes still in somewhat reasonable time.

--- day_14/solution.py
+++ day_14/solution.py
@@ -15,3 +15,17 @@ while len(scores) < limit:
 
 print("Day 14, part 1:", scores[-10:])
 
+looking_for = str(puzzle_input)
+pos = scores.find(looking_for)
+while pos < 0:
+	i = len(scores) - len(looking_for)
+	for _ in range(puzzle_input):
+		a = int(scores[elf_a])
+		b = int(scores[elf_b])
+		scores += str(a + b)
+		elf_a = (elf_a + a + 1) % len(scores)
+		elf_b = (elf_b + b + 1) % len(scores)
+	pos = scores.find(looking_for, i)
+
+print("Day 14, part 2:", pos)
+
