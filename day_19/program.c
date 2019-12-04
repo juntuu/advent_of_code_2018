c08d012f6a6d53d376f3b796388b4034feea4715
Juho Eerola <juho.eerola@tuni.fi>
2019-12-05 01:48:24 +0200

    Day 19: simplifying
    Assuming all zero registers, except r 0, which is taken as a
    parameter.

--- day_19/program.c
+++ day_19/program.c
@@ -2,32 +2,14 @@
 
 typedef unsigned long T;
 
-T program(T *r) {
-	T n = r[2] + 2;
-	n *= n;
-	n *= 19;
-	n *= 11;
+T program(T bigger) {
+	T n = 905;
+	if (bigger) n += 10550400;
 
-	T temp = r[5] + 3;
-	temp *= 22;
-	temp += 3;
-	n += temp;
-
-	if (r[0]) {
-		temp = 27;
-		temp *= 28;
-		temp += 29;
-		temp *= 30;
-		temp *= 14;
-		temp *= 32;
-		n += temp;
-	}
-
-	T sum = 0;
-	for (T i = 1; i <= n; i++)
+	T sum = n;
+	for (T i = 1; i < n; i++)
 		if ((n % i) == 0)
 			sum += i;
-
 	return sum;
 }
 
